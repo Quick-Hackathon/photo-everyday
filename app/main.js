9f8216baf93c44d32597ef95fb6ad02bfd4593f3
John Jablonski <john.jablonski@eventstag.com>
2018-10-14 16:32:56 +0100

    Simple electron app with dev tools

--- app/main.js
+++ app/main.js
@@ -1,22 +1,48 @@
 const { app, BrowserWindow } = require("electron");
+const isElectronDev = require("electron-is-dev");
+const {
+    default: installExtension,
+    REACT_DEVELOPER_TOOLS,
+    REDUX_DEVTOOLS
+} = require("electron-devtools-installer");
 
 // disable security warnings, useful but annoying
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = "true";
 
+/**
+ * @type {BrowserWindow}
+ */
 let mainWindow;
 
 const createWindow = () => {
-  mainWindow = new BrowserWindow({
-    show: true,
-    height: 600,
-    width: 800
-  });
+    mainWindow = new BrowserWindow({
+        show: true,
+        height: 600,
+        width: 800
+    });
 
-  mainWindow.loadURL("https://google.com");
+    mainWindow.loadURL("https://google.com");
+    mainWindow.on("show", mainWindowOnShow);
+};
+
+const initDevTools = () => {
+    mainWindow.webContents.openDevTools();
+
+    [REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS].forEach(extension => {
+        installExtension(extension)
+            .then(name => console.log(`Added Extension: ${name}`))
+            .catch(err => console.log("An error occurred: ", err));
+    });
+};
+
+const mainWindowOnShow = () => {
+    mainWindow.maximize();
 };
 
 app.on("ready", () => {
-  console.log("App ready, creating main window");
-  // createMenu();
-  createWindow();
+    console.log("App ready, creating main window");
+    createWindow();
+    if (isElectronDev) {
+        initDevTools();
+    }
 });
