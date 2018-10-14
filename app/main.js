0a18edf6410696445831270ff1c43a36251540f4
Viktor Hubert <rpgmorpheus@gmail.com>
2018-10-14 16:37:50 +0100

    config reducer

--- app/main.js
+++ app/main.js
@@ -6,17 +6,17 @@ process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = "true";
 let mainWindow;
 
 const createWindow = () => {
-  mainWindow = new BrowserWindow({
-    show: true,
-    height: 600,
-    width: 800
-  });
+    mainWindow = new BrowserWindow({
+        show: true,
+        height: 600,
+        width: 800
+    });
 
-  mainWindow.loadURL("https://google.com");
+    mainWindow.loadURL("http://localhost:3000/");
 };
 
 app.on("ready", () => {
-  console.log("App ready, creating main window");
-  // createMenu();
-  createWindow();
+    console.log("App ready, creating main window");
+    // createMenu();
+    createWindow();
 });
