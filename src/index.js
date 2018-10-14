972ecc51f9d39cba049b939a5341984f88b08508
Viktor Hubert <rpgmorpheus@gmail.com>
2018-10-14 16:29:15 +0100

    Added react-redux

--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { createStore, compose } from "redux";
+import rootReducer from "./store/rootReducer";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const defaultConfig = {
+    test: "ok"
+};
+
+const defaultState = {
+    config: defaultConfig
+};
+
+const store = createStore(rootReducer, defaultState, composeEnhancers());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
